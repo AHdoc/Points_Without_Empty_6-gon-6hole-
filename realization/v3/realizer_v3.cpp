deaab36b7e97d75f14fcc7344a64e24c00456e30
AHdoc <zhangyibo12342000@gmail.com>
2020-09-20 23:51:56 +0200

    A correct realizer_v3 with stupid search stategies.

--- realization/v3/realizer_v3.cpp
+++ realization/v3/realizer_v3.cpp
@@ -6,11 +6,85 @@
 #include<random>
 #include<vector>
 #include<set>
+#include<queue>
 #include<algorithm>
 #include<assert.h>
 using namespace std;
 
+namespace ahdoc{
+	typedef long long LL;
+	
+	#define x first
+	#define y second
+	#define c1 first.first
+	#define c2 first.second
+	#define c3 second
+	#define MP3(a,b,c) make_pair(make_pair(a,b),c)
+	
+	bool on_the_left(pair<LL,LL> a,pair<LL,LL> b,pair<LL,LL> c){return (b.x-a.x)*(c.y-a.y)-(c.x-a.x)*(b.y-a.y)>0;}
+	
+	vector<pair<LL,LL>> pt;
+	vector<queue<pair<pair<int,int>,int>>> Q;
+	vector<pair<pair<int,int>,int>> C;
+	
+	bool proceed(int i,int j){
+		//cerr<<">   i="<<i<<"   j="<<j<<"\n";
+		while(!Q[i].empty() && on_the_left(pt[Q[i].front().c1],pt[i],pt[j])){
+			if(proceed(Q[i].front().c1,j)) return true;
+			auto tmp=Q[i].front();
+			if(tmp.c1>C[i].c1) C[i].c1=tmp.c1;
+			if(tmp.c2>C[i].c2) C[i].c2=tmp.c2;
+			if(tmp.c3>C[i].c3) C[i].c3=tmp.c3;
+			Q[i].pop();
+		}
+		if(C[i].c3!=-1 && on_the_left(pt[C[i].c3],pt[j],make_pair(0LL,0LL))) return true;
+		Q[j].push(MP3(i,C[i].c1,C[i].c2));
+		//cerr<<"<   i="<<i<<"   j="<<j<<"\n";
+		//for(int k=0;k<pt.size();k++){
+		//	cerr<<"       Q["<<k<<"]={ ";
+		//	for(auto x:Q[k]) cerr<<"("<<x.c1<<","<<x.c2<<","<<x.c3<<") ";
+		//	cerr<<"}\n";
+		//	cerr<<"       C["<<k<<"]=("<<C[k].c1<<","<<C[k].c2<<","<<C[k].c3<<")\n";
+		//}
+		return false;
+	}
+	
+	bool solve(vector<pair<LL,LL>> _pt){
+		pt=_pt;
+		int n=pt.size();
+		//cerr<<"n="<<n<<"\n";
+		//for(int i=0;i<n;i++) cerr<<"   "<<i<<": ("<<pt[i].x<<","<<pt[i].y<<")\n";
+		Q.clear(); C.clear(); Q.resize(n); C.resize(n);
+		for(int i=0;i<n;i++)
+			C[i].c1=C[i].c2=C[i].c3=-1;
+		for(int i=0;i+1<n;i++)
+			if(proceed(i,i+1))
+				return true;
+		return false; 
+	}
+	
+	bool find6hole(vector<pair<LL,LL>> pt,pair<LL,LL> p){
+		int n=pt.size();
+		for(int i=0;i<n;i++){
+			pt[i].x-=p.x;
+			pt[i].y-=p.y;
+		}
+		sort(pt.begin(),pt.end(),[](pair<LL,LL> p1,pair<LL,LL> p2){return atan2(p1.y,p1.x)<atan2(p2.y,p2.x);});
+		if(solve(pt)) return true;
+		for(int i=0;i<n;i++){
+			pt[i].x*=-1;
+			pt[i].y*=-1;
+		}
+		sort(pt.begin(),pt.end(),[](pair<LL,LL> p1,pair<LL,LL> p2){return atan2(p1.y,p1.x)<atan2(p2.y,p2.x);});
+		if(solve(pt)) return true;
+		return false;
+	}
+}
+
 typedef long long LL;
+
+#define x first
+#define y second
 
 LL absLL(LL x){return (x<0?-x:x);}
 LL gcd(LL a,LL b){return (b==0?a:gcd(b,a%b));}
@@ -30,12 +104,13 @@ int n;
 LL tot_achievement,achievement[MAXN+1];
 LL radius[MAXN+1],lvl[MAXN+2];
 
-#define x first
-#define y second
-
 bool on_the_left(pair<LL,LL> a,pair<LL,LL> b,pair<LL,LL> c){return (b.x-a.x)*(c.y-a.y)-(c.x-a.x)*(b.y-a.y)>0;}
+bool on_the_line(pair<LL,LL> a,pair<LL,LL> b,pair<LL,LL> c){return (b.x-a.x)*(c.y-a.y)-(c.x-a.x)*(b.y-a.y)==0;}
 
-bool find6hole(vector<pair<LL,LL>> pt,pair<LL,LL> p){}
+bool find6hole(vector<pair<LL,LL>> pt,pair<LL,LL> p){
+	double ret=ahdoc::find6hole(pt,p);
+	return ret;
+}
 
 bool check(int i,int ii,vector<pair<LL,LL>> pt,pair<LL,LL> p){
 	set<pair<LL,LL>> ang;
@@ -58,14 +133,67 @@ bool check(int i,int ii,vector<pair<LL,LL>> pt,pair<LL,LL> p){
 		for(int j=1;j<i;j++)
 			if(j!=i-1 && !on_the_left(p,pt[j-1],pt[i-2])) return false;
 	}
-	return find6hole(pt,p);
+	return !find6hole(pt,p);
+}
+
+void check_no6hole(vector<pair<LL,LL>> pt){ // Check whether or not the given set of points contains no 6-hole
+	int n=pt.size();
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++){
+			set<int> pts={i,j};
+			if(pts.size()!=2) continue;
+			if(pt[i].x==pt[j].x && pt[i].y==pt[j].y){
+				cerr<<"Two points coincide.\n";
+				exit(1);
+			}
+		}
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			for(int k=0;k<n;k++){
+				set<int> pts={i,j,k};
+				if(pts.size()!=3) continue;
+				if(on_the_line(pt[i],pt[j],pt[k])){
+					cerr<<"Three points which are colinear.\n";
+					exit(1);
+				}
+			}
+	int a[6];
+	for(a[0]=0;a[0]<n;a[0]++) for(a[1]=0;a[1]<n;a[1]++) for(a[2]=0;a[2]<n;a[2]++) for(a[3]=0;a[3]<n;a[3]++) for(a[4]=0;a[4]<n;a[4]++) for(a[5]=0;a[5]<n;a[5]++){
+		set<int> pts={a[0],a[1],a[2],a[3],a[4],a[5]};
+		if(pts.size()!=6) continue;
+		bool is6hole=true;
+		for(int i=0;i<6;i++){
+			int j=(i+1)%6;
+			for(int k=(j+1)%6;k!=i;k=(k+1)%6)
+				if(!on_the_left(pt[a[i]],pt[a[j]],pt[a[k]])){ is6hole=false; break; }
+			if(!is6hole) break;
+		}
+		for(int k=0;k<n;k++){
+			bool chk=true;
+			for(int i=0;i<6;i++) if(a[i]==k){ chk=false; break; }
+			if(!chk) continue;
+			bool chkout=false;
+			for(int i=0;i<6;i++){
+				int j=(i+1)%6;
+				if(!on_the_left(pt[a[i]],pt[a[j]],pt[k])){ chkout=true; break; }
+			}
+			if(!chkout){ is6hole=false; break; }
+		}
+		if(is6hole){
+			cerr<<"n="<<n<<"\n";
+			for(int k=0;k<n;k++) cerr<<"   Point #"<<k+1<<": ("<<pt[k].x<<","<<pt[k].y<<")\n";
+			cerr<<"A 6-hole:\n";
+			for(int i=0;i<6;i++) cerr<<"   "<<a[i]<<": ("<<pt[a[i]].x<<","<<pt[a[i]].y<<")\n";
+			exit(1);
+		}
+	}
 }
 
 void dfs(int i,int ii,vector<pair<LL,LL>> pt){ // points numbered from ii to i are of the same level
 	++tot_achievement; ++achievement[i-1];
-	if(tot_achievement%1000000==0){
+	if(tot_achievement%10000==0){
 		cerr<<"achieve = "<<tot_achievement<<"     ";
-		for(int j=11;j<=30;j++){
+		for(int j=10;j<=30;j++){
 			if(achievement[j]==0) break;
 			cerr<<j<<":"<<achievement[j]<<" ";
 		}
@@ -74,14 +202,16 @@ void dfs(int i,int ii,vector<pair<LL,LL>> pt){ // points numbered from ii to i a
 	
 	if(i==n+1){
 		for(int i=1;i<=n;i++) cerr<<"Point #"<<i<<": ("<<pt[i-1].x<<","<<pt[i-1].y<<")\n";
+		check_no6hole(pt);
 		exit(0);
 	}
 	
-	for(int t=1;t<=100;t++){
+	for(int t=1;t<=20;t++){
 		pair<LL,LL> p={random(-radius[i],radius[i]),random(-radius[i],radius[i])}; 
 		if(check(i,ii,pt,p)){
 			vector<pair<LL,LL>> pt2=pt;
 			pt2.push_back(p);
+			//check_no6hole(pt2);
 			dfs(i+1,lvl[i]==lvl[i+1]?ii:i+1,pt2);
 		}
 	}
@@ -105,10 +235,18 @@ void Realizer(string pat){
 	cerr<<"lvl[0]="<<lvl[0]<<"   lvl[n+1]="<<lvl[n+1]<<"\n";
 	
 	tot_achievement=0; for(int i=1;i<=30;i++) achievement[i]=0;
-	for(;;) dfs(2,lvl[1]==lvl[2]?1:2,{{0LL,0LL}}); // must fix the kernel point
+	//for(;;) dfs(2,lvl[1]==lvl[2]?1:2,{{0LL,0LL}}); // must fix the kernel point
+	for(;;) dfs(4,lvl[3]==lvl[4]?1:4,{{-5LL,-5LL},{5LL,-5LL},{0LL,5LL}});
 }
 
 int main(){
-	Realizer("3477710");
+	Realizer("3333330");
+	//Realizer("8730");
+	//Realizer("88510");
+	//Realizer("3477710");
+	
+	//check({{0,0},{59,-35},{-99,81},{-77,6},{16,-87},{96,-82}});
+	//cout<<ahdoc::find6hole({{0,0},{59,-35},{-99,81},{-77,6},{16,-87},{96,-82}},{92,-73})<<"\n";
+	//check({{0,0},{59,-35},{-99,81},{-77,6},{16,-87},{96,-82},{92,-73}});
 }
 
